9484fa442c6aa2a71cd973b2df700ad9e76e9e17
chadalad <cnuckols7@gmail.com>
2020-07-08 18:56:51 -0400

    init commit

--- actions.js
+++ actions.js
@@ -4,4 +4,39 @@ const axios = require('axios');
 const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
 
 /* YOUR CODE GOES HERE */
-module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
+const gotDesserts = (desserts) => ({
+  type: GOT_DESSERTS_DATA,
+  desserts,
+});
+
+const addDessert = (newDessert) => ({
+  type: NEW_DESSERT_DATA,
+  newDessert,
+});
+
+const fetchDesserts = (dispatch) => {
+  dispatch(() => {
+    return axios.get('/api/desserts')
+      .then((res) => {
+        const { data : { desserts } } = res;
+
+        dispatch({
+          type: GOT_DESSERTS_DATA,
+          desserts: desserts,
+        })
+      })
+
+      
+  })
+}
+
+const postDessert = () => ({
+
+});
+
+module.exports = { 
+  gotDesserts, 
+  addDessert, 
+  fetchDesserts, 
+  postDessert 
+};
